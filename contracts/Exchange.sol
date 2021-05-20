9217d954572792d3391eafd71843ce43482144ae
hansebastian <sebastian_hans@hotmail.com>
2021-05-21 00:35:37 +0800

    Add more attributes to exchange contract

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -3,11 +3,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract exchange {
+  enum TransactionTypes { EtherToErc, ErcToEther }
 
   // Make use of SafeMath to do calculation that will not be result in overflow / incorrectness
   using SafeMath for uint;
 
   uint256 amtEtherTotal = 0 ether;
   uint256 amtErc20Total = 0 ether;
-  uint 
+  // TODO: Change this arbitrary value to something that makes more sense 
+  uint256 exchangeRateEtherToErc = 0.5 ether;
+
+  struct Transaction {
+    address user;
+    TransactionType transactionType;
+    uint256 transactionRate;
+    uint256 amtEther;
+    uint256 amtErc;
+    uint256 date;
+  }
 }
