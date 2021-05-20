a05b5e4ce6b78cb54651edc34637ef9ddcfce513
hansebastian <sebastian_hans@hotmail.com>
2021-05-21 00:26:40 +0800

    Add exchange contract and safemath

---
+++ contracts/Exchange.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract exchange {
+
+  // Make use of SafeMath to do calculation that will not be result in overflow / incorrectness
+  using SafeMath for uint;
+
+  uint256 amtEtherTotal = 0 ether;
+  uint256 amtErc20Total = 0 ether;
+  uint 
+}
