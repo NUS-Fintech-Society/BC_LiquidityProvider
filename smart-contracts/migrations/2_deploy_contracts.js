14c9ac45185be88074aacbece4881a1d70faf9bc
hansebastian <sebastian_hans@hotmail.com>
2021-06-10 15:29:31 +0800

    Update deploy contract to use two inputs

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -1,9 +1,12 @@
-const ERC20 = artifacts.require("ERC20");
+const ERC20Improved = artifacts.require("ERC20Improved");
 const Exchange = artifacts.require("Exchange");
 
-module.exports = async function(deployer, network, accounts) {
-	let deployAccount = accounts[0];
-    let erc20 = await deployer.deploy(ERC20, {from: deployAccount})
-    await deployer.deploy(Exchange, ERC20.address, {from: deployAccount});
-
-} 
+module.exports = async function (deployer, network, accounts) {
+  let deployAccount = accounts[0];
+  let erc20 = await deployer.deploy(ERC20Improved, "NAME1", "SYMBOL1", {
+    from: deployAccount,
+  });
+  await deployer.deploy(Exchange, ERC20Improved.address, {
+    from: deployAccount,
+  });
+};
